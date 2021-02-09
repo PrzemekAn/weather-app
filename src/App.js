124cd830791898af280d5ab422d8440bd6c17797
PrzemekAn <przemekandrzejczak@gmail.com>
2021-02-09 18:09:41 +0100

    date added, error handler added

--- src/App.js
+++ src/App.js
@@ -10,14 +10,37 @@ class App extends Component {
       temperature: '',
       city: 'Poznan',
       background: '',
+      date: '',
     }
   
-  getWeather(){
-    
-    fetch(`${api.base}weather?q=${this.state.city}&appid=${api.key}`)
-    .then(res => res.json())
-    .then(result => {
 
+  getDate() {
+    const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    const currentDate = new Date();
+    const day = currentDate.getDay();
+    const date = currentDate.getDate();
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+
+    this.setState({
+      date:`${days[day-1]}, ${date}-${months[month]}-${year}`,
+    })
+
+  }
+    
+  getWeather(){
+    this.getDate();
+    fetch(`${api.base}weather?q=${this.state.city}&appid=${api.key}`)
+    .then(res => {
+      // console.log(res)
+      return (res.json())})
+
+    .then(result => {
+      // console.log(result)
+      if(result.cod !== 200){
+        return(alert('There\'s no such city'))
+      }
       if(Math.round(result.main.temp-273.15)>10){
         this.setState({
            background: ' warm'
@@ -32,32 +55,33 @@ class App extends Component {
   }
 
 
-  
   componentDidMount(){
     this.getWeather();
+  }
 
-    // if(this.state.temperature > 10){
-    //   this.setState({
-    //     background: ' cold',
-    //   })
-    // }
-    // else{
-    //   this.setState({
-    //     background: ' warm',
-    //   })
-    // }
+  changeHandler(e) {
+    this.setState({
+      city: e.target.value,
+    })
+  }
+
+  searchSubmit = (e) => {
+
+    if(e.keyCode === 13){
+      this.getWeather();
+    }
 
   }
 
   render(){
-    console.log(this.state.temperature)
+    // console.log(this.state.temperature)
   return (
     
     <div className="App">
       <main className = {'wrapper' + this.state.background}>
-          <input type="text" placeholder = 'Search...' className = 'location-search'/>
-          <div className="location-info">Poznań</div>
-          <div className="date">07.02.2021</div>
+          <input type="text" placeholder = 'Search...' className = 'location-search' onChange = {this.changeHandler.bind(this)} onKeyUp = {this.searchSubmit}/>
+          <div className="location-info">{this.state.city}</div>
+          <div className="date">{this.state.date}</div>
           <div className="weather-data">{this.state.temperature}<sup>o</sup>C</div>
       </main>
     </div>
